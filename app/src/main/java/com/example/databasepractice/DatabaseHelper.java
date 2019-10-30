69a2819a87a457f9eea94847f3ec2492e7c6140a
mondragon1375 <mondragon1375@gmail.com>
2019-10-30 13:49:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/databasepractice/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.databasepractice;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "student.db";    // Name of the Database
+    public static final String TABLE_NAME = "student_table";    // Name of the Table storing the data
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";          // This is the name of each individual Column
+    public static final String COL_3 = "SURNAME";       // Each are label to what they are storing
+    public static final String COL_4 = "GRADE";
+
+    public DatabaseHelper(Context context) {
+        // Whenever this constructor is called the database is created
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // This
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
