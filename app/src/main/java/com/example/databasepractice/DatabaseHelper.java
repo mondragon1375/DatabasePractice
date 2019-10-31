c3839718b2ddbc64e31790b27853821bb36d5c1a
mondragon1375 <mondragon1375@gmail.com>
2019-10-31 13:49:35 -0500

    video 2&3 done

--- app/src/main/java/com/example/databasepractice/DatabaseHelper.java
+++ app/src/main/java/com/example/databasepractice/DatabaseHelper.java
@@ -1,10 +1,9 @@
 package com.example.databasepractice;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "student.db";    // Name of the Database
@@ -15,17 +14,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_4 = "GRADE";
 
     public DatabaseHelper(Context context) {
-        // Whenever this constructor is called the database is created
+        // Whenever this constructor is called the database and table is created
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // This
+        // This will create the table for the database
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT, SURNAME TEXT, GRADE INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insertData(String name, String surname, String grade){
+        // This method will allow to data to be entered to the database
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contetValues = new ContentValues();
+        contetValues.put(COL_2, name);
+        contetValues.put(COL_3, surname);
+        contetValues.put(COL_4, grade);
+        long result = db.insert(TABLE_NAME, null, contetValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
     }
 }
--- app/src/main/java/com/example/databasepractice/MainActivity.java
+++ app/src/main/java/com/example/databasepractice/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.databasepractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText editName, editSurname, editGrade;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText)findViewById(R.id.editText_name);
+        editSurname = (EditText)findViewById(R.id.editText_surname);
+        editGrade = (EditText)findViewById(R.id.editText_grade);
+        btnAddData = (Button)findViewById(R.id.button_add);
+
+        addData();
+    }
+
+    public void addData(){
+        btnAddData.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        boolean isInstered = myDb.insertData(editName.getText().toString(),
+                                editSurname.getText().toString(),
+                                editGrade.getText().toString());
+                        if (isInstered = true)
+                            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Data is not Inserted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
