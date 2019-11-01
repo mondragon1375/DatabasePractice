a62741d7fecda38d417e3935b69eaf7a65e41822
mondragon1375 <mondragon1375@gmail.com>
2019-11-01 13:48:03 -0500

    video 4 done

--- app/src/main/java/com/example/databasepractice/DatabaseHelper.java
+++ app/src/main/java/com/example/databasepractice/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.databasepractice;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -26,6 +27,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This is necessary but i don't know why
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
     }
@@ -42,5 +44,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         else
             return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME,null);
+        return res;
     }
 }
--- app/src/main/java/com/example/databasepractice/MainActivity.java
+++ app/src/main/java/com/example/databasepractice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.databasepractice;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
     EditText editName, editSurname, editGrade;
-    Button btnAddData;
+    Button btnAddData, btnViewAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
         editSurname = (EditText)findViewById(R.id.editText_surname);
         editGrade = (EditText)findViewById(R.id.editText_grade);
         btnAddData = (Button)findViewById(R.id.button_add);
+        btnViewAll = (Button)findViewById(R.id.button_view);
 
         addData();
+        viewAll();
     }
 
     public void addData(){
@@ -42,5 +46,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    public void viewAll(){
+        btnViewAll.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Cursor res = myDb.getAllData();
+                        if (res.getCount() == 0) {
+                            showMessage("Error", "Nothing Found");
+                            return;
+                        }
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()){
+                            buffer.append("Id: " + res.getString(0) + "\n");
+                            buffer.append("Name: " + res.getString(1) + "\n");
+                            buffer.append("Surname: " + res.getString(2) + "\n");
+                            buffer.append("Grade: " + res.getString(3) + "\n\n");
+                        }
+                        showMessage("Data", buffer.toString());
+                    }
+
+                }
+        );
+    }
+
+    public void showMessage (String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+
     }
 }
